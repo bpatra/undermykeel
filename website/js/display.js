4c6bf527fbff3d2fdb8301baac7fc45eb28ec8cd
benoit <benoit.patra@gmail.com>
2013-07-03 23:49:25 +0200

    add switch logic for tide from high-low to low-high add dedicate page for chart rendering

--- website/js/display.js
+++ website/js/display.js
@@ -1,7 +1,19 @@
-﻿$(document).ready(function () {
+﻿var descendant = true
+$(document).ready(function () {
     $('#fromgroundzero').click(function () {
         $("section#from_groundzero").toggle('slow');
         $("section#from_onepoint").toggle('slow');
+    });
+
+    $("#switchtide").click(function () {
+        var elt1 = $("section#tide_selection").children().eq(0);
+        var elt2 = $("section#tide_selection").children().eq(1);
+        elt2.insertBefore(elt1);
+        descendant = !descendant;
+    });
+
+    $("#compute").click(function () {
+        window.location.href = "tide.html"
     });
 
 });
