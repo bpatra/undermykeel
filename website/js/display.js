c62f4d195e10a3b773168f2bc05f363eb1808053
benoit <benoit.patra@gmail.com>
2013-07-03 19:45:23 +0200

    adding jQuery and toggle from groound zero section

---
+++ website/js/display.js
@@ -0,0 +1,7 @@
+﻿$(document).ready(function () {
+    $('#fromgroundzero').click(function () {
+        $("section#from_groundzero").toggle('slow');
+        $("section#from_onepoint").toggle('slow');
+    });
+
+});
