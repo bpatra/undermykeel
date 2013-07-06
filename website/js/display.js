3ed87929fc0fe666b4b0b3fae509789a20eda746
benoit <benoit.patra@gmail.com>
2013-07-06 20:26:30 +0200

    fix tide calculation, remove depths use tidals instead, add  hover on graph.

--- website/js/display.js
+++ website/js/display.js
@@ -1,5 +1,4 @@
-﻿var isDescendant = true;
-$(document).ready(function () {
+﻿$(document).ready(function () {
     $('#fromgroundzero').click(function () {
         $("section#from_groundzero").toggle('slow');
         $("section#from_onepoint").toggle('slow');
@@ -32,39 +31,78 @@ function ComputeGraph() {
     }
     var lowTideTime = timeToFloat($("#lowtide_time").val());
     var highTideTime = timeToFloat($("#hightide_time").val());
-    var lowTideValue = $("#lowtide_value").val();
-    var highTideValue = $("#hightide_value").val();
+    var M = parseFloat($("#tidal_value").val());
     var localPointTime = timeToFloat($("#onepoint_time").val());
-    var localPointValue = $("#onepoint_value").val();
+    var localPointValue = parseFloat($("#onepoint_value").val());
 
     //TODO add validation.
-
-    var D = Math.abs(highTideTime - lowTideTime);
-    var M = highTideValue - lowTideValue;
+    var isEbb = isEbbTide(highTideTime, lowTideTime);
+    var D = isEbb ? smartDuration(highTideTime ,lowTideTime) : smartDuration(lowTideTime, highTideTime);
     var data = [];
-    var start  = isDescendant ? highTideTime : lowTideTime;
+    var start = isEbb ? highTideTime : lowTideTime;
+    var h0 = hFromt(D, M, localPointTime - start) - localPointValue;
     for (var i = 0; i < 1000; i++) {
         var t1 = i * D / 1000;
-        data.push({ x: start + t1, y: hFromt(D, M, t1) });
+        var t2 = start + t1 - highTideTime;
+        data.push({ x: start + t1, y: hFromt(D, M, t2) + h0 });
     }
 
     var graph = new Rickshaw.Graph({
         element: document.querySelector("#chart"),
         width: 580,
         height: 250,
+		renderer: 'line',
         series: [{
             color: 'steelblue',
-            data: data
+            data: data,
+            name: 'depth'
         }]
+    });
+
+    var hoverDetail = new Rickshaw.Graph.HoverDetail({
+        graph: graph,
+        xFormatter: function(x) { return floatToTime(x)},
+        yFormatter: function (y) { return y.toFixed(2) + " m" }
+    });
+
+    var axes = new Rickshaw.Graph.Axis.Time({
+        graph: graph
     });
 
     graph.render();
 
 }
 
-function hFromt(D, M, t){
-    var y = Math.sin(Math.PI*t/(2*D));
-    return M*y*y;
+Number.prototype.mod = function (n) {
+    return ((this % n) + n) % n;
+}
+
+function smartDuration(start, end) {
+    var duration = end - start;
+    return (Math.floor(duration).mod(24)) + (duration - Math.floor(duration));
+}
+
+function isEbbTide(highTideTime, lowTideTime) {
+    var m1 = smartDuration(highTideTime, lowTideTime);
+    var m2 = smartDuration(lowTideTime,highTideTime);
+    return m1 < m2; 
+}
+
+function hFromt(D, M, t, isEbb){
+    var y = Math.sin(Math.PI * t / (2 * D));
+    return M * y * y;
+}
+
+function floatToTime(value) {
+    var hours = Math.floor(value);
+    var minutes = Math.floor((value - Math.floor(value)) * 60);
+    if(minutes < 10){
+        minutes = '0'+minutes;
+    }
+    if(hours < 10) {
+        hours = '0' + hours;
+    }
+    return  hours+ ':' + minutes;
 }
 
 function timeToFloat(timeString) {
@@ -73,4 +111,13 @@ function timeToFloat(timeString) {
     var minutes = parseFloat(splitted[1]);
 
     return hours + minutes / 60.0;
+}
+
+function ValidateInput() {
+    var lowTideTime = timeToFloat($("#lowtide_time").val());
+    var highTideTime = timeToFloat($("#hightide_time").val());
+    var M = parseFloat($("#tidal_value").val());
+    var localPointTime = timeToFloat($("#onepoint_time").val());
+    var localPointValue = parseFloat($("#onepoint_value").val());
+
 }
