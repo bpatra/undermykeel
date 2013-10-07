082e0d871c251db56d24042cc5059c3d5d87b365
benoit <benoit.patra@gmail.com>
2013-10-07 08:58:04 +0200

    Revert "prepare ground for allow calculus using a reference tide"
    This reverts commit 9c147be9a9fc2bd0167fa41c3ac40702bbd5114a.

--- website/js/lang.js
+++ website/js/lang.js
@@ -48,12 +48,7 @@
 	
 	LocalData:{
         'en':'Local data',
-        'fr':'Données du lieu'
-    },
-	
-	LocalDataRef:{
-        'en':'Local data for reference tide',
-        'fr':'Données du lieu pour la marée de référence'
+        'fr':'Données locales'
     },
 	
 	LocalTime:{
@@ -64,21 +59,6 @@
 	LocalDepth:{
 		'en':'Depth',
 		'fr':'Profondeur'
-	},
-	
-	CoeffCheckBox:{
-		'en':'Compute using a reference tide',
-		'fr':'Calcul à partir d\'une marée de référence'
-	},
-	
-	Coeffs:{
-		'en':'Coefficient',
-		'fr':'Coefficient'
-	},
-	
-	DepthAtLowTide:{
-		'en':'Depth at low tide',
-		'fr':'Profondeur à basse mer '
 	}
 };
 
