ac14b8137ac73d4f7231a05d9403f0a45d2b8c81
benoit <benoit.patra@gmail.com>
2013-10-07 21:26:40 +0200

    powershell script for application cache update

---
+++ website/UpdateApplicationCache.ps1
@@ -0,0 +1,4 @@
+﻿$lines = Get-Content .\undermykeel.appcache
+$lines[1] = "#" + (Get-Date).ToString()
+$newappCache = New-Item -Name "undermykeel.appcache" -type file -Force -Verbose
+$lines |ForEach-Object {Add-Content -Value $_ -Path $newappCache} 
